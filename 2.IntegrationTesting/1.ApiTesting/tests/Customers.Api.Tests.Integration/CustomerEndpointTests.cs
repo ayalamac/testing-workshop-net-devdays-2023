734bd54f727697a4c2aaa70c7cf0b4e87758d33f
Nick Chapsas <nick.chapsas@gmail.com>
2023-10-23 16:31:31 +0200

    setup done

--- 2.IntegrationTesting/1.ApiTesting/tests/Customers.Api.Tests.Integration/CustomerEndpointTests.cs
+++ 2.IntegrationTesting/1.ApiTesting/tests/Customers.Api.Tests.Integration/CustomerEndpointTests.cs
@@ -1,36 +1,39 @@
 using System.Net;
-using System.Net.Http.Json;
+using Bogus;
 using Customers.Api.Contracts.Requests;
 using Customers.Api.Contracts.Responses;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
 namespace Customers.Api.Tests.Integration;
 
+[Collection("Shared collection")]
 public class CustomerEndpointTests : IAsyncLifetime
 {
-    private readonly WebApplicationFactory<ICustomerApiMarker> _waf = new();
-
     private readonly HttpClient _client;
-    private readonly List<Guid> _idsToDelete = new();
+    private readonly Func<Task> _resetDb;
+    private readonly Action<string> _setupGitHubUser;
 
-    public CustomerEndpointTests()
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.FullName, f => f.Person.FullName)
+        .RuleFor(x => x.Email, f => f.Person.Email)
+        .RuleFor(x => x.DateOfBirth, f => f.Person.DateOfBirth.Date)
+        .RuleFor(x => x.GitHubUsername, f => f.Person.UserName.Replace(".", "").Replace("-", "").Replace("_", ""));
+
+    public CustomerEndpointTests(CustomersApiFactory waf)
     {
-        _client = _waf.CreateClient();
+        _client = waf.Client;
+        _resetDb = waf.ResetDatabaseAsync;
+        _setupGitHubUser = waf.RegisterGitHubUser;
     }
     
     [Fact]
     public async Task Create_ShouldCreateCustomer_WhenDetailsAreValid()
     {
         // Arrange
-        var request = new CustomerRequest
-        {
-            Email = "nick@dometrain.com",
-            FullName = "Nick Chapsas",
-            DateOfBirth = new DateTime(1993, 01, 01),
-            GitHubUsername = "nickchapsas"
-        };
+        var request = _customerGenerator.Generate();
+        _setupGitHubUser(request.GitHubUsername);
 
         var expectedResponse = new CustomerResponse
         {
@@ -42,13 +45,12 @@ public class CustomerEndpointTests : IAsyncLifetime
 
         // Act
         var response = await _client.PostAsJsonAsync("customers", request);
-        var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
-        _idsToDelete.Add(customerResponse!.Id);
         
         // Assert
+        var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
         response.StatusCode.Should().Be(HttpStatusCode.Created);
         customerResponse.Should().BeEquivalentTo(expectedResponse, x => x.Excluding(e => e.Id));
-        customerResponse.Id.Should().NotBeEmpty();
+        customerResponse!.Id.Should().NotBeEmpty();
         response.Headers.Location.Should().Be($"http://localhost/customers/{customerResponse.Id}");
     }
 
@@ -56,21 +58,15 @@ public class CustomerEndpointTests : IAsyncLifetime
     public async Task Get_ShouldReturnCustomer_WhenCustomerExists()
     {
         // Arrange
-        var request = new CustomerRequest
-        {
-            Email = "nick@dometrain.com",
-            FullName = "Nick Chapsas",
-            DateOfBirth = new DateTime(1993, 01, 01),
-            GitHubUsername = "nickchapsas"
-        };
+        var request = _customerGenerator.Generate();
+        _setupGitHubUser(request.GitHubUsername);
 
         var createCustomerResponse = await _client.PostAsJsonAsync("customers", request);
         var createdCustomerResponse = await createCustomerResponse.Content.ReadFromJsonAsync<CustomerResponse>();
-        _idsToDelete.Add(createdCustomerResponse!.Id);
         
         var expectedResponse = new CustomerResponse
         {
-            Id = createdCustomerResponse.Id,
+            Id = createdCustomerResponse!.Id,
             Email = request.Email,
             FullName = request.FullName,
             DateOfBirth = request.DateOfBirth,
@@ -85,14 +81,123 @@ public class CustomerEndpointTests : IAsyncLifetime
         var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
         customerResponse.Should().BeEquivalentTo(expectedResponse);
     }
+    
+    [Fact]
+    public async Task Create_ShouldReturnBadRequest_WhenTheEmailIsInvalid()
+    {
+        // Arrange
+        var request = _customerGenerator.Clone()
+            .RuleFor(x => x.Email, _ => "nick")
+            .Generate();
+        _setupGitHubUser(request.GitHubUsername);
+
+        // Act
+        var response = await _client.PostAsJsonAsync("customers", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+        problemDetails!.Errors["Email"].Should().Equal("nick is not a valid email address");
+    }
+    
+    [Fact]
+    public async Task GetAll_ShouldReturnAllCustomers_WhenCustomersExist()
+    {
+        // Arrange
+        var request = _customerGenerator.Generate();
+        _setupGitHubUser(request.GitHubUsername);
+    
+        var createCustomerHttpResponse = await _client.PostAsJsonAsync("customers", request);
+        var createdCustomer = await createCustomerHttpResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+
+        // Act
+        var response = await _client.GetAsync("customers");
+
+        // Assert
+        var customerResponse = await response.Content.ReadFromJsonAsync<GetAllCustomersResponse>();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        customerResponse!.Customers.Should().ContainEquivalentOf(createdCustomer).And.HaveCount(1);
+    }
+    
+    [Fact]
+    public async Task Get_ShouldReturnNotFound_WhenCustomerDoesNotExist()
+    {
+        // Arrange
+        var customerId = Guid.NewGuid();
+
+        // Act
+        var response = await _client.GetAsync($"customers/{customerId}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+    
+    [Fact]
+    public async Task Update_ShouldUpdateCustomerDetails_WhenDetailsAreValid()
+    {
+        // Arrange
+        var createRequest = _customerGenerator.Generate();
+        _setupGitHubUser(createRequest.GitHubUsername);
+    
+        var createCustomerHttpResponse = await _client.PostAsJsonAsync("customers", createRequest);
+        var createdCustomer = await createCustomerHttpResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+        
+        var updateRequest = _customerGenerator.Generate();
+        _setupGitHubUser(updateRequest.GitHubUsername);
+        
+        var updatedResponse = new CustomerResponse
+        {
+            Id = createdCustomer!.Id,
+            Email = updateRequest.Email,
+            FullName = updateRequest.FullName,
+            DateOfBirth = updateRequest.DateOfBirth,
+            GitHubUsername = updateRequest.GitHubUsername
+        };
+
+        // Act
+        var response = await _client.PutAsJsonAsync($"customers/{createdCustomer.Id}", updateRequest);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);    
+        var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+        customerResponse.Should().BeEquivalentTo(updatedResponse);
+    }
+    
+    [Fact]
+    public async Task Delete_ShouldDeleteCustomer_WhenCustomerExists()
+    {
+        // Arrange
+        var request = _customerGenerator.Generate();
+        _setupGitHubUser(request.GitHubUsername);
+    
+        var createCustomerHttpResponse = await _client.PostAsJsonAsync("customers", request);
+        var createdCustomer = await createCustomerHttpResponse.Content.ReadFromJsonAsync<CustomerResponse>();
+
+        // Act
+        var response = await _client.DeleteAsync($"customers/{createdCustomer!.Id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task Delete_ShouldReturnNotFound_WhenCustomerDoesNotExist()
+    {
+        // Arrange
+        var customerId = Guid.NewGuid();
+
+        // Act
+        var response = await _client.DeleteAsync($"customers/{customerId}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
 
     public Task InitializeAsync() => Task.CompletedTask;
 
     public async Task DisposeAsync()
     {
-        foreach (var id in _idsToDelete)
-        {
-            await _client.DeleteAsync($"customers/{id}");
-        }
+        await _resetDb();
     }
 }
